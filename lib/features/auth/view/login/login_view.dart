8ed05806351a2c4a4347e4751f30f7769132ccce
kashpour <ibrahimkashpour@yahoo.com>
2024-10-24 20:51:24 +0200

    log in layout commit

--- lib/features/auth/view/login/login_view.dart
+++ lib/features/auth/view/login/login_view.dart
@@ -7,13 +7,13 @@ class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: mainColor,
+      backgroundColor: Colors.white,
       appBar: AppBar(
         automaticallyImplyLeading: false,
       ),
-      body: const Column(
+      body: Column(
         children: [
-          Text(
+          const Text(
             'Welcome\nBack!',
             style: TextStyle(
               fontSize: 32.0,
@@ -21,13 +21,118 @@ class LoginView extends StatelessWidget {
               fontStyle: FontStyle.italic,
             ),
           ),
-          Text(
+          const Text(
             'Hey! Good to see you again',
             style: TextStyle(
               fontSize: 18.0,
               color: Colors.white,
               fontWeight: FontWeight.bold,
               fontStyle: FontStyle.italic,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      hintText: 'Email',
+                      hintStyle: const TextStyle(color: Colors.black),
+                      floatingLabelBehavior: FloatingLabelBehavior.always,
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 20.0, horizontal: 10.0),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12)),
+                      suffixIcon: const Icon(
+                        Icons.email,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      hintText: 'Password',
+                      hintStyle: const TextStyle(color: Colors.black),
+                      floatingLabelBehavior: FloatingLabelBehavior.always,
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 20.0, horizontal: 10.0),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12)),
+                      suffixIcon: const Icon(
+                        Icons.visibility_off_outlined,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: mainColor,
+                        fixedSize: Size(MediaQuery.of(context).size.width, 50)),
+                    onPressed: () {},
+                    child: const Text(
+                      'Log in',
+                      style: TextStyle(color: Colors.white, fontSize: 20.0),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      GestureDetector(
+                        onTap: () {},
+                        child: const Text(
+                          'Forget Password?',
+                          style: TextStyle(
+                              color: mainColor, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(40),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Don't have an account!",
+                        style: TextStyle(
+                          fontSize: 16.0,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 8.0,
+                      ),
+                      GestureDetector(
+                          onTap: () {},
+                          child: const Text(
+                            'Sign Up',
+                            style: TextStyle(
+                                fontSize: 18.0,
+                                color: mainColor,
+                                fontWeight: FontWeight.bold),
+                          ))
+                    ],
+                  ),
+                )
+              ],
             ),
           )
         ],
